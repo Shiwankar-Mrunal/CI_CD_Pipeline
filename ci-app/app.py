0102c7c6054bcc6884be5009ea69595a35cda344
Shiwankar-Mrunal <shiwankar.mrunal99@gmail.com>
2025-10-03 10:26:33 +0530

    Update app.py

--- ci-app/app.py
+++ ci-app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask
+import os
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -6,4 +8,5 @@ def hello():
     return "Hello from Azure Web App!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000)
+    port = int(os.environ.get("PORT", 8000))  # Use Azure PORT or fallback to 8000
+    app.run(host="0.0.0.0", port=port)
