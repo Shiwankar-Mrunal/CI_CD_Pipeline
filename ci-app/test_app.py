1d15ae04735d1c1715c55a00e26eb407f76ae430
Shiwankar-Mrunal <shiwankar.mrunal99@gmail.com>
2025-10-07 05:48:51 +0000

    fixed

--- ci-app/test_app.py
+++ ci-app/test_app.py
@@ -1,9 +1,9 @@
 import pytest
-import app
-from ci-app.app 
+from app import app
 
 def test_hello():
     tester = app.test_client()
     response = tester.get("/")
     assert response.status_code == 200
     assert b"Hello from Azure Web App!" in response.data
+
