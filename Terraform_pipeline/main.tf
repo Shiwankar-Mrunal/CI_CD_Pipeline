436a09ca06a313810f917dd84d535d5f8e655039
shiwankar-Mrunal <shiwankar.mrunal99@gmail.com>
2025-10-08 16:08:28 +0530

    Initial commit

--- Terraform_pipeline/main.tf
+++ Terraform_pipeline/main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "MY-RG"{
 resource "azurerm_container_registry" "my-acr"{
     name = var.myarc
     resource_group_name = azurerm_resource_group.MY-RG.name
-    location = azurerm_resource_group.My-RG.location
+    location = var.azurerm_resource_group.My-RG.location
 
     #Stock keeping unit
     sku = "Basic"
