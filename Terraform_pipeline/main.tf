55c526c16ade91773f39e43e246aa9b004076c04
shiwankar-Mrunal <shiwankar.mrunal99@gmail.com>
2025-10-08 15:11:21 +0530

    Initial commit

--- Terraform_pipeline/main.tf
+++ Terraform_pipeline/main.tf
@@ -20,7 +20,7 @@ resource "azurerm_container_registry" "my-acr"{
 }
 
 module "application" {
-  source = "../modules"
+  source = "/modules"
   name1 = var.azurerm_resource_group.MY-RG.name
   location1 = var.azurerm_resource_group.MY-RG.location
   app_service_plan_name     = "var.app_service_plan_name"
