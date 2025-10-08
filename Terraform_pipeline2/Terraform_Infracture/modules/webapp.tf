58d3dbc82716621c3d11e60f95480c9fbe389710
shiwankar-Mrunal <shiwankar.mrunal99@gmail.com>
2025-10-08 10:50:43 +0530

    add files

---
+++ Terraform_pipeline2/Terraform_Infracture/modules/webapp.tf
@@ -0,0 +1,19 @@
+resource "azurerm_app_service_plan" "app1"{
+      name  =  var.app_service_plan_name
+      location = var.azurerm.resource_group_location.myloc
+      resource_group_name = var.azurerm_resource_group.myrg
+      kind = "Windows"
+      reserved = true
+
+      sku {
+        tier = "Basic"
+        size = "B1"
+      }
+}
+  
+resource "azure_app_service" "my_web_app1"{
+   name = unique_web_app
+   resource_group_name = var.azurerm_resource_group.name
+   location = var.azurerm.resource_group_location.location
+   app_service_plan_id = azurerm_app_service_plan.app1.id
+}
